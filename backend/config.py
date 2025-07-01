d41bb410ddae08386b60d5bb5aef7a08b938309b
Elijah M <elijah.macharia@student.moringaschool.com>
2025-07-01 16:45:49 +0300

    project

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv ('DATABASE_URL', 'sqlite:///app.db') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your-secret-key'
 
